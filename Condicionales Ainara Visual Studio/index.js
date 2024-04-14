c3f48cc676ec2103cc70ec166c8c5beff97960bb
AinaraBritJacob <166017112+AinaraBritJacob@users.noreply.github.com>
2024-04-14 16:22:48 -0300

    Add files via upload
    Condicionales en Visual studio ejemplos.

---
+++ Condicionales Ainara Visual Studio/index.js
@@ -0,0 +1,41 @@
+let Nombrealumno = Bastian
+if (Nombrealumno = Bastian) {
+    alert("Aprobado")
+} else {
+    alert("Desaprobado")
+}
+
+let Tengohambre = true
+if (Tengohambre = true) {
+    alert("Comer")
+} else {
+    alert("No comer")
+}
+
+let Preciogomitas = Number(prompt("¿Cuanto salen las gomitas?"))
+if (Preciogomitas = 300) {
+    alert("Comprar gomitas")
+} else {
+    alert("No comprar gomitas")
+}
+
+let Nombreprofesor = prompt("¿Que materia das?")
+if (Nombreprofesor = Biologia ) {
+    alert("Tenemos Biologia")
+} else {
+    alert("No tenemos Biologia")
+}
+
+let Edad = Number(prompt("¿Cuantos años tienes?"))
+if (Edad = 5) {
+alert("Terminas jardin")
+} else {
+    alert("No terminas jardin")
+}
+
+let Tengo_calor= true
+if(Tengo_calor= true) {
+    alert("Ponerme remera manga corta")
+} else {
+    alert("Abrigarme")
+}
